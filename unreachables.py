d7940dc1c8c0ebb1d0c26ff616c5cb52b018a50b
killbit <74510491+xkillbit@users.noreply.github.com>
2022-05-09 11:34:52 -0400

    Ugly script

---
+++ unreachables.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python3
+
+print("Unreachable hosts:")
+with open('all_given_ips.txt','r') as f:
+    full_list = f.readlines()
+    print("FULL LIST: {}".format(full_list))
+    with open('all_up.txt','r') as upfile:
+        up_list = upfile.readlines()
+        print("UPHOSTS LIST: {} ".format(up_list))
+        for ip in full_list:
+            if ip not in up_list:
+                print(ip)
+        print("FULL LIST: {}".format(full_list))
+        print("UPHOSTS LIST: {} ".format(up_list))
+        print("FULL LIST: {}".format(len(full_list)))
+        print("UP HOSTS: {}".format(len(up_list)))
+        print("DIFFERENCE: {}".format(len(full_list)-len(up_list)))
